975671b82a68cd7c533721764e2fe897ed0529a8
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-15 20:02:03 -0700

    passed all tests

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 module.exports = {
   calcTax: function (income) {
     if ( income < 11 ) return ( income * 0.1 );
-    if ( income < 21 ) return ( ((income-10) * 0.07) + 0.1 );
-    if ( income < 31 ) return ( ((income-20) * 0.05) + 0.17 );
-    return ( ((income-30) * 0.03) + 0.22 );
+    if ( income < 21 ) return ( ((income-10) * 0.07) + 1 ).toFixed(2);
+    if ( income < 31 ) return ( ((income-20) * 0.05) + 1.7 ).toFixed(2);
+    return ( ((income-30) * 0.03) + 2.2 ).toFixed(2);
   }
-}
+};
--- test/test.js
+++ test/test.js
@@ -3,18 +3,21 @@ var expect = require('chai').expect;
 
 describe('Tax Calculator', function(){
 
-  it('should tax 10% on $0-10', function() {
+  it('tier 1: 10%', function() {
     expect(code.calcTax(1)).to.equal(0.1);
     expect(code.calcTax(10)).to.equal(1);
   });
-  it('should tax 7% on $11-20 + tier 1 taxes (10%)', function(){
-    expect(code.calcTax(20)).to.equal(1.7);
+  it('tier 2: 7% + 10%', function(){
+    expect(code.calcTax(15)).to.equal('1.35');
+    expect(code.calcTax(20)).to.equal('1.70');
   });
-  it('should tax 5% on $21-30 + tier 1 & 2 taxes (10 & 7%)', function(){
-    expect(code.calcTax(30)).to.equal(2.2);
+  it('tier 3: 5% + 17%', function(){
+    expect(code.calcTax(25)).to.equal('1.95');
+    expect(code.calcTax(30)).to.equal('2.20');
   });
-  it('should tax 3% on everything above $31 + tier 1, 2, & 3 taxes (10, 7, & 5%)', function(){
-    expect(code.calcTax(40)).to.equal(2.5);
+  it('tier 4: 3% + 22%', function(){
+    expect(code.calcTax(35)).to.equal('2.35');
+    expect(code.calcTax(40)).to.equal('2.50');
+    expect(code.calcTax(100)).to.equal('4.30');
   });
-
 });
